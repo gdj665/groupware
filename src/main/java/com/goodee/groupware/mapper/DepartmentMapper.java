95301916f8724294aa8c247c3cf49774a983f1fe
권원중 <126855396+wonjung-KWON@users.noreply.github.com>
2023-08-16 17:30:52 +0900

    {권원중} - 부서리스트 출력 mapper, service, controller 기본 로직 구현

--- src/main/java/com/goodee/groupware/mapper/DepartmentMapper.java
+++ src/main/java/com/goodee/groupware/mapper/DepartmentMapper.java
@@ -1,5 +1,12 @@
 package com.goodee.groupware.mapper;
 
-public interface DepartmentMapper {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DepartmentMapper {
+	// 부서 계층형 출력
+	List<Map<String,Object>> getDepartmentList();
 }
--- src/main/java/com/goodee/groupware/sevice/DepartmentService.java
+++ src/main/java/com/goodee/groupware/sevice/DepartmentService.java
@@ -1,5 +1,32 @@
 package com.goodee.groupware.sevice;
 
-public class DepartmentService {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goodee.groupware.mapper.DepartmentMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional
+@Slf4j
+public class DepartmentService {
+	@Autowired
+	private DepartmentMapper departmentMapper;
+	
+	// 부서 리스트
+	public Map<String,Object> getDepartmentList(){
+		List<Map<String,Object>> departmentList = departmentMapper.getDepartmentList();
+		// departmentList 디버깅
+		log.debug("DepartmentService.getDepartmentList()-->" + departmentList);
+		
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		resultMap.put("department", departmentList);
+		return resultMap;
+	}
 }
