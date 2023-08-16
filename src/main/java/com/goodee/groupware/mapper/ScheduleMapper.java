8e6a26073062e40edbfa218957adfd1198015410
hong <dinpyo@naver.com>
2023-08-16 17:30:17 +0900

    (홍진표)전체 일정 조회 구현 중

--- src/main/java/com/goodee/groupware/mapper/ScheduleMapper.java
+++ src/main/java/com/goodee/groupware/mapper/ScheduleMapper.java
@@ -1,5 +1,16 @@
 package com.goodee.groupware.mapper;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.goodee.groupware.vo.Schedule;
+
+@Mapper
 public interface ScheduleMapper {
 
+	
+	List<Schedule> selectScheduleListByMonth(Map<String, Object> paramMap);
+	
 }
--- src/main/java/com/goodee/groupware/vo/Schedule.java
+++ src/main/java/com/goodee/groupware/vo/Schedule.java
@@ -1,5 +1,17 @@
 package com.goodee.groupware.vo;
 
-public class Schedule {
+import lombok.Data;
 
+@Data
+public class Schedule {
+	private int scheduleNo;
+	private String memberId;
+	private String scheduleCategory;
+	private String scheduleTitle;
+	private String scheduleContent;
+	private String scheduleBegindate;
+	private String scheduleEnddate;
+	private String createdate;
+	private String updatedate;
+	
 }
