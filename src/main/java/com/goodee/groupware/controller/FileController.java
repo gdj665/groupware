5e5051d176b8d6530b905940c12ffde32f3f7f6b
DWJ15910 <116102250+DWJ15910@users.noreply.github.com>
2023-08-22 08:59:44 +0900

    (조동욱) 게시판 수정사항 추가
    1. 게시글 입력 폼에서 선택한 첨부파일 명 출력
    2. 게시글 입력 폼에서 파일이 사이즈 초과시 경고 메세지 출력
    3. 파일 다운로드 pdf명도 정상 다운로드 가능
     - ISO-8859-1 인코딩

--- src/main/java/com/goodee/groupware/controller/FileController.java
+++ src/main/java/com/goodee/groupware/controller/FileController.java
@@ -20,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.goodee.groupware.mapper.FileMapper;
 import com.goodee.groupware.vo.BoardFile;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class FileController {
 	@Autowired
@@ -43,9 +46,11 @@ public class FileController {
 				
 				// 파일 다운로드를 위한 헤더 생성
 				HttpHeaders headers = new HttpHeaders();
-				// 생성한 헤더에 빌더 객체를 생성한 뒤 어떤 이름으로 다운받을지(filename 설정) 설정합니다
-				headers.setContentDisposition(ContentDisposition.builder("attachment").filename(boardFile.getBoardFileOri()).build());
-				
+				log.debug("boardFile.getBoardFileType-->"+boardFile.getBoardFileType());
+				String boardFileOriName = boardFile.getBoardFileOri();
+				// 파일 명을 ISO-8859-1 타입으로 바꾸어서 인코딩 오류 해결 (PDF이름 정상 출력)
+				String encodedFileName = new String(boardFileOriName.getBytes("UTF-8"), "ISO-8859-1");
+				headers.setContentDisposition(ContentDisposition.builder("attachment").filename(encodedFileName).build());
 				System.out.println("FileDownloadRest-->게시판 파일 다운로드 성공");
 				// 성공적으로 다운로드되면 F12를 누르고 네트워크탭에서 Status확인을 통해 세부사항 확인가능 (Status 200이면 성공!)
 				return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
